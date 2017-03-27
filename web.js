33e8ea6886dfa93d2840e2189208c963ccf63942
JorisPe <joris_pelleriaux5@hotmail.com>
2017-03-27 19:53:41 +0200

    ready

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
 var express = require('express');
+var packageInfo = require('./package.json');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-  
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var server = app.listen(3000, "0.0.0.0", function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
 });
+
+module.exports = function (bot) {
+  app.post('/' + bot.token, function (req, res) {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+};
